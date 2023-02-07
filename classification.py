89a013b404650083c04ba9d570d8869c221fa0bd
wanyingtian <wanying.tian@outlook.com>
2023-02-07 13:55:53 -0800

    processed data

--- classification.py
+++ classification.py
@@ -8,3 +8,20 @@ from tensorflow.keras.layers import Dense, InputLayer
 from sklearn.metrics import classification_report
 from tensorflow.keras.utils import to_categorical
 import numpy as np
+
+## Loading Data
+data = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+print(data.info())
+print(Counter(data['DEATH_EVENT']))
+y = data['DEATH_EVENT']
+x = data[['age','anaemia','creatinine_phosphokinase','diabetes','ejection_fraction','high_blood_pressure','platelets','serum_creatinine','serum_sodium','sex','smoking','time']]
+
+## Data Pre-processing
+x = pd.get_dummies(x)
+# split datasets to train and test
+X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=0.2,random_state=1)
+# standardize numerical values
+ct = ColumnTransformer([("numeric",StandardScaler(),['age','creatinine_phosphokinase','ejection_fraction','platelets','serum_creatinine','serum_sodium','time'])])
+# apply standardization to training and testing features
+X_train = ct.fit_transform(X_train)
+X_test = ct.fit_transform(X_test)
