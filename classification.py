75a44550b669d5a069d9825f42ca5a5338d4caab
wanyingtian <wanying.tian@outlook.com>
2023-02-08 13:24:58 -0800

    plotted loss and accuracy

--- classification.py
+++ classification.py
@@ -3,11 +3,13 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from collections import Counter
 from sklearn.compose import ColumnTransformer
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, InputLayer
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, InputLayer
 from sklearn.metrics import classification_report
-from tensorflow.keras.utils import to_categorical
+from keras.utils import to_categorical
 import numpy as np
+import matplotlib.pyplot as plt
 
 ## Loading Data
 data = pd.read_csv('heart_failure_clinical_records_dataset.csv')
@@ -47,11 +49,33 @@ model.add(Dense(2,activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 ## Train and evaluate
-model.fit(X_train, Y_train, epochs=100, batch_size=16, verbose=1)
-loss, acc = model.evaluate(X_test, Y_test, verbose = 0)
-print("Loss", loss, "Accuracy:", acc)
+history = model.fit(X_train, Y_train, epochs=100, batch_size=16, verbose=1, validation_split=0.2)
+val_loss, val_acc = model.evaluate(X_test, Y_test, verbose = 0)
+print("Loss", val_loss, "Accuracy:", val_acc)
 
 y_estimate = model.predict(X_test, verbose = 0)
 y_estimate = np.argmax(y_estimate, axis=1)
 y_true = np.argmax(Y_test, axis=1)
 print(classification_report(y_true, y_estimate))
+
+fig = plt.figure()
+# plot loss and accuracy over each epoch
+
+ax1 = fig.add_subplot(2, 1, 1)
+ax1.plot(history.history['accuracy'])
+ax1.plot(history.history['val_accuracy'])
+ax1.set_title('model accuracy')
+ax1.set_ylabel('Accuracy')
+ax1.set_xlabel('epoch')
+ax1.legend(['train', 'validation'], loc='upper left')
+
+
+# Plot loss and val_loss over each epoch
+ax2 = fig.add_subplot(2, 1, 2)
+ax2.plot(history.history['loss'])
+ax2.plot(history.history['val_loss'])
+ax2.set_title('model loss')
+ax2.set_ylabel('loss')
+ax2.set_xlabel('epoch')
+ax2.legend(['train', 'validation'], loc='upper left')
+plt.show()
